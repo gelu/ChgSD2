47fad18a4d1d1d502b9cab8db0c0b512193d7065
/dev/rsa <rsa@shwamp.murmansk.ru>
2010-01-12 21:21:49 +0300

    Small sql fixes

--- addition/717_culling_of_stratholme_mangos.sql
+++ addition/717_culling_of_stratholme_mangos.sql
@@ -21,15 +21,15 @@ update `creature_template` set `AIName`='', `Scriptname`='boss_lord_epoch' where
 update `creature_template` set `AIName`='', `Scriptname`='boss_malganis' where `entry` in (26533);
 update `instance_template` set `script` = 'instance_culling_of_stratholme' WHERE map=595;
 -- Spawning Salramm in the instance --
-INSERT INTO creature VALUES (4458725,26530,595,1,1,0,0,2174.32,1307.32,131.866,4.32264,25,0,0,337025,62535,0,0);
+INSERT INTO creature VALUES (4458724,26530,595,1,1,0,0,2174.32,1307.32,131.866,4.32264,25,0,0,337025,62535,0,0);
 -- heroic version --
 INSERT INTO creature VALUES (4458725,26530,595,2,1,0,0,2178.93,1307.89,131.526,4.12069,25,0,0,421281,62535,0,0);
 -- Spawning Meathook in the instance --
-INSERT INTO creature VALUES (4458739,26529,595,1,1,0,0,2351.43,1218.03,130.078,4.64621,25,0,0,337025,0,0,0);
+INSERT INTO creature VALUES (4458738,26529,595,1,1,0,0,2351.43,1218.03,130.078,4.64621,25,0,0,337025,0,0,0);
 -- heroic version --
 INSERT INTO creature VALUES (4458739,26529,595,2,1,0,0,2350.42,1215.31,130.23,4.76314,25,0,0,421281,0,0,0);
 -- Spawning Lord Epoch in the instance --
-INSERT INTO creature VALUES (4458741,26532,595,1,1,0,1820,2446.25,1111.97,148.077,3.37779,25,0,0,337025,41690,0,0);
+INSERT INTO creature VALUES (4458740,26532,595,1,1,0,1820,2446.25,1111.97,148.077,3.37779,25,0,0,337025,41690,0,0);
 -- heroic version --
 INSERT INTO creature VALUES (4458741,26532,595,2,1,0,1820,2446.17,1111.64,148.076,3.25517,25,0,0,421281,41690,0,0);
 -- Spawning Malganis in the instance
@@ -54,7 +54,7 @@ DELETE FROM `gameobject_template` WHERE (`entry`=187723);
 INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `castBarCaption`, `faction`, `flags`, `size`, `data0`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`, `data12`, `data13`, `data14`, `data15`, `data16`, `data17`, `data18`, `data19`, `data20`, `data21`, `data22`, `data23`, `ScriptName`) VALUES (187723, 0, 3631, 'Crusaders\' Square Gate', '', 1375, 6553632, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '');
 
 update `gameobject` set `state` = '1' where `id` in (187711);
-update `gameobject` set `spawntimesecs` = '-604800', `state` = '0', `faction` = '0' where `id` in (190663);
+update `gameobject` set `spawntimesecs` = '-604800', `state` = '0'  where `id` in (190663);
 
 UPDATE `creature` set `curhealth` = '8600' where `id`=28167;
 UPDATE `creature` set `curhealth` = '8600' where `id`=28169;
