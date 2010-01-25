802da0724f24063c1ad2e2722800cb61d5564060
/dev/rsa <kris13@bk.ru>
2010-01-25 19:12:30 +0300

    Violet hold SQL patch

--- addition/716_the_violet_hold_mangos.sql
+++ addition/716_the_violet_hold_mangos.sql
@@ -45,6 +45,7 @@ DELETE FROM `spell_script_target` WHERE `entry` IN (54160,59474);
 INSERT INTO spell_script_target VALUES (54160, 1, 29266);
 INSERT INTO spell_script_target VALUES (59474, 1, 29266);
 
-UPDATE creature_template SET ScriptName="npc_door_seal_vh", unit_flags=33816580 WHERE entry=30896;
-UPDATE creature_template SET faction_A=35, faction_H=35 WHERE entry=30658;
-UPDATE creature_template SET faction_A=1720, faction_H=1720, ScriptName="mob_vh_dragons", minlevel=70,maxlevel=70,minhealth=7000,maxhealth=8000  WHERE entry IN (30660, 30661, 30662, 30663, 30664, 30666, 30667, 30668, 32191, 30695);
+UPDATE `creature_template` SET `ScriptName`='npc_door_seal_vh', unit_flags=33816580 WHERE entry=30896;
+UPDATE `creature_template` SET `faction_A`=35, faction_H=35 WHERE entry=30658;
+UPDATE `creature_template` SET `faction_A`=1720, faction_H=1720, ScriptName='mob_vh_dragons', minlevel=70,maxlevel=70,minhealth=7000,maxhealth=8000  WHERE entry IN (30660, 30661, 30662, 30663, 30664, 30666, 30667, 30668, 32191, 30695);
+
