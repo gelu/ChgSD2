1aac0043e102d700c766944bafc039afc4407f72
/dev/rsa <kris13@bk.ru>
2010-05-13 23:52:22 +0400

    Process-safe database command execution (test).

--- include/sc_boss_spell_worker.cpp
+++ include/sc_boss_spell_worker.cpp
@@ -4,6 +4,7 @@
 #include "precompiled.h"
 #include "sc_boss_spell_worker.h"
 #ifdef DEF_BOSS_SPELL_WORKER_H
+#include "ace/Process_Mutex.h"
 
 BossSpellWorker::BossSpellWorker(ScriptedAI* bossAI)
 {
@@ -45,13 +46,18 @@ void BossSpellWorker::_resetTimer(uint8 m_uiSpellIdx)
 
 void BossSpellWorker::LoadSpellTable()
 {
+    // mutex block for process-safe request execute
+    ACE_Process_Mutex mMutex = ACE_Process_Mutex("BSW_Lock");
+
     debug_log("BSW: Loading table of spells boss  %u difficulty %u", bossID , currentDifficulty);
 
       char query[MAX_QUERY_LEN];
 
       sprintf(query, "SELECT entry, spellID_N10, spellID_N25, spellID_H10, spellID_H25, timerMin_N10, timerMin_N25, timerMin_H10, timerMin_H25, timerMax_N10, timerMax_N25, timerMax_H10, timerMax_H25, data1, data2, data3, data4, locData_x, locData_y, locData_z, varData, StageMask_N, StageMask_H, CastType, isVisualEffect, isBugged, textEntry FROM `boss_spell_table` WHERE entry = %u;\r\n", bossID);
 
+    mMutex.acquire();
       QueryResult* Result = strSD2Pquery(query);
+    mMutex.release();
 
     if (Result)
     {
