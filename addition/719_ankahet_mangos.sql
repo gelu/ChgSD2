8e88c1a774b7d246aee52faf5e9e3b13c02f67b6
/dev/rsa <kris13@bk.ru>
2010-06-30 13:24:06 +0400

    Small correct Ahn'kahet sql

--- addition/719_ankahet_mangos.sql
+++ addition/719_ankahet_mangos.sql
@@ -108,7 +108,8 @@ INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`, `equip
 (30391, 619, 2, 1, 0, 0, 350.138, -958.988, -79.4422, 2.16229, 30, 0, 0, 1, 0, 0, 0),
 (30391, 619, 2, 1, 0, 0, 342.879, -944.352, -79.8533, 1.64157, 30, 0, 0, 1, 0, 0, 0);
 
-UPDATE creature_template SET `AIname`='EventAI' WHERE `entry` = 30176;
+UPDATE `creature_template` SET `AIname`='EventAI' WHERE `entry` = 30176;
+DELETE FROM `creature_ai_scripts` WHERE `creature_id` = 30176;
 INSERT INTO `creature_ai_scripts` (`id`, `creature_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_type`, `action1_param1`, `action1_param2`, `action1_param3`, `action2_type`, `action2_param1`, `action2_param2`, `action2_param3`, `action3_type`, `action3_param1`, `action3_param2`, `action3_param3`, `comment`) VALUES
 ('3017610', '30176', '11', '0', '100', '6', '0', '0', '0', '0', '11', '56151', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', ''),
 ('3017611', '30176', '8', '0', '100', '6', '56153', '0', '0', '0', '28', '0', '56153', '0', '0', '0', '0', '0', '0', '0', '0', '0', '');
