94da73260361733e534a00812d16d03b560c8f04
/dev/rsa <kris13@bk.ru>
2010-08-21 10:48:14 +0400

    To core changes in 10380

--- scripts/eastern_kingdoms/stranglethorn_vale.cpp
+++ scripts/eastern_kingdoms/stranglethorn_vale.cpp
@@ -191,8 +191,7 @@ bool ChooseReward_npc_riggle_bassbait(Player* pPlayer, Creature* pCreature, cons
         DoScriptText(SAY_WINNER, pCreature,pPlayer);
         pCreature->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER);
         ((npc_riggle_bassbaitAI*)(pCreature->AI()))->bEventWinnerFound = true;
-        // right way to load another creature, you need first argument because he uses it to get map.
-        Creature* creature2 = pCreature->GetMap()->GetCreature(MAKE_NEW_GUID(54687,15078,HIGHGUID_UNIT));
+        Creature* creature2 = GetClosestCreatureWithEntry(pCreature,15087,60.0f);
         if (creature2)
         {
             creature2->SetFlag(UNIT_NPC_FLAGS,UNIT_NPC_FLAG_QUESTGIVER);
