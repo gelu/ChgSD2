287cc2288573704a5b159843e580e31b223e74be
x3n <system.x3n@gmail.com>
2011-03-23 07:23:02 +0100

    [IOV68] rewrite Gundrak boss Colossus
    Signed-off-by: Janu <jaed1@gmx.net>

--- sql/custom/068_01_mangos_creature_template.sql
+++ sql/custom/068_01_mangos_creature_template.sql
@@ -21,7 +21,9 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_scarlet_miner' WHERE `entry` 
 
 -- Instance gundrak
 
-UPDATE `creature_template` SET `ScriptName` = 'mob_colossus_elemental' WHERE `entry` = '29573'; -- boss colossus mob colossus elemental
+UPDATE `creature_template` SET `ScriptName` = 'boss_colossus' WHERE `entry` = '29307';
+UPDATE `creature_template` SET `ScriptName` = 'boss_elemental' WHERE `entry` = '29573';
+UPDATE `creature_template` SET `ScriptName` = 'npc_living_mojo' WHERE `entry` = '29830';
 
 -- Thargold Ironwing
 
